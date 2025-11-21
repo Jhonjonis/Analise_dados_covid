3a5763e527dc0908f241e37a68202b60da037a6c
Isabela Santana <isabelasantanadiaslima@gmail.com>
2025-11-21 16:16:55 -0300

    Delete scripts directory

--- scripts/run_jetl.sh
+++
@@ -1,5 +0,0 @@
-
-#!/bin/bash
-docker exec -it spark-master spark-submit \
-  --master spark://spark-master:7077 \
-  /opt/spark/app/jobs/etl_covid.py
--- scripts/run_wordcount.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker exec -it spark-master spark-submit \
-  --master spark://spark-master:7077 \
-  /opt/spark/app/jobs/wordcount.py
