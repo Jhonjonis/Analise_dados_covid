40203cfacb84639f38a763f88cc01f916eb7db2e
Isabela Santana <isabelasantanadiaslima@gmail.com>
2025-11-25 15:00:49 -0300

    Update entrypoint.sh

--- bigdata/spark/entrypoint.sh
+++ bigdata/spark/entrypoint.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-# Fail fast
-set -e
+export PYTHONPATH="/opt/spark/app:/opt/spark/app/src:$PYTHONPATH"
 
+# Iniciar 
 if [ "$SPARK_MODE" = "master" ]; then
-    echo "Inicializando Spark MASTER..."
     /opt/spark/sbin/start-master.sh
 elif [ "$SPARK_MODE" = "worker" ]; then
-    echo "Inicializando Spark WORKER..."
     /opt/spark/sbin/start-worker.sh $SPARK_MASTER_URL
-else
-    echo "ERRO: SPARK_MODE deve ser 'master' ou 'worker'"
-    exit 1
 fi
 
-# Mantém o container rodando
+# Manter  container funcionando
 tail -f /dev/null
